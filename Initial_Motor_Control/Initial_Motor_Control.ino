ed985c9cd4bea42fb5c731a905e1086cadc004b0
DESKTOP-GKF2608\walke <walkergrimes5@gmail.com>
2018-10-31 09:04:05 -0500

    Updated to spin all four wheels

--- Initial_Motor_Control/Initial_Motor_Control.ino
+++ Initial_Motor_Control/Initial_Motor_Control.ino
@@ -1,43 +1,88 @@
 
 // SPEED CONTROL PINS
-const int pwmA = 2 ;  // Initializing pin 2 as pwm
-const int pwmB = 3 ;  // Initializing pin 3 as pwm
 
+  // Front
+  const int pwm_L_frnt = 2 ;  // Initializing pin 2 as pwm
+  const int pwm_R_frnt = 3 ;  // Initializing pin 3 as pwm
+  // Rear
+  const int pwm_L_rear = 5 ;  // Initializing pin 4 as pwm
+  const int pwm_R_rear = 4 ;  // Initializing pin 5 as pwm
+  
 // LOGIC PINS
-const int A_1 = 52 ; // Initializing Motor A pin one 
-const int A_2 = 50 ; // Initializing Motor A pin two 
-const int B_1 = 48 ; // Initializing Motor B pin one 
-const int B_2 = 46 ; // Initializing Motor B pin two 
+  // Front
+  const int L_frnt_1 = 52 ; // Initializing Left Front pin one 
+  const int L_frnt_2 = 50 ; // Initializing Left Front pin two 
+  const int R_frnt_1 = 48 ; // Initializing Right Front pin one 
+  const int R_frnt_2 = 46 ; // Initializing Right Front pin two 
+  
+  // Rear
+  const int L_rear_1 = 44 ; // Initializing Left Rear pin one 
+  const int L_rear_2 = 42 ; // Initializing Left Rear pin two 
+  const int R_rear_1 = 40 ; // Initializing Right Rear pin one 
+  const int R_rear_2 = 38 ; // Initializing Right Rear pin two 
 
-// Power L298N 
-const int pwr = 11 ;
-
+// Power Pins L298N 
+  const int pwr_frnt = 11 ; // Front Power
+  const int pwr_rear = 13 ; // Rear Power
+  
 void setup() { 
-pinMode(pwmA,OUTPUT) ;   // Set PWM1 pin as output
-pinMode(pwmB,OUTPUT) ;   // Set PWM2 pin as output
 
-pinMode(A_1,OUTPUT) ;    // Set all logic pins to output
-pinMode(A_2,OUTPUT) ;
-pinMode(B_1,OUTPUT) ;
-pinMode(B_2,OUTPUT) ;
+pinMode(pwm_L_frnt,OUTPUT) ;   // Front logic pins
+pinMode(pwm_R_frnt,OUTPUT) ;  
 
-pinMode(pwr,OUTPUT) ;
+pinMode(pwm_L_rear,OUTPUT) ;   // Rear logic pins
+pinMode(pwm_R_rear,OUTPUT) ;   
+
+pinMode(L_frnt_1,OUTPUT) ;    // Set all front logic pins to output
+pinMode(L_frnt_2,OUTPUT) ;
+pinMode(R_frnt_1,OUTPUT) ;
+pinMode(R_frnt_2,OUTPUT) ;
+
+pinMode(L_rear_1,OUTPUT) ;    // Set all rear logic pins to output
+pinMode(L_rear_2,OUTPUT) ;
+pinMode(R_rear_1,OUTPUT) ;
+pinMode(R_rear_2,OUTPUT) ;
+
+
+pinMode(pwr_frnt,OUTPUT) ;
+pinMode(pwr_rear,OUTPUT) ;
+
 }
+
+void frntLeftFWD(){
+// Spin the front left wheel forward (ccw viewed from back of wheel)
+digitalWrite(L_frnt_1,LOW) ;
+digitalWrite(L_frnt_2,HIGH) ;
+analogWrite(pwm_L_frnt,150);
+}
+
+void frntRightFWD(){
+// Spin the front right wheel forward (cw viewed from back of wheel)
+digitalWrite(R_frnt_1,LOW) ;
+digitalWrite(R_frnt_2,HIGH) ;
+analogWrite(pwm_R_frnt,150);
+}
+
+void rearLeftFWD(){
+// Spin the rear left wheel forward (ccw viewed from back of wheel)
+digitalWrite(L_rear_1,LOW) ;
+digitalWrite(L_rear_2,HIGH) ;
+analogWrite(pwm_L_rear,150);
+}
+
+void rearRightFWD(){
+// Spin the front right wheel forward (cw viewed from back of wheel)
+digitalWrite(R_rear_1,LOW) ;
+digitalWrite(R_rear_2,HIGH) ;
+analogWrite(pwm_R_rear,150);
+}
+
 
 void loop() {
-// Spin the front left wheel ccw viewed from back of wheel
-digitalWrite(pwr,HIGH) ;
-digitalWrite(A_1,HIGH) ;
-digitalWrite(A_2,LOW) ;
-analogWrite(pwmA,150);
+digitalWrite(pwr_frnt,HIGH) ;
+digitalWrite(pwr_rear,HIGH) ;
+frntRightFWD();
+frntLeftFWD();
+rearLeftFWD();
+rearRightFWD();
 }
-
-/*
-void forward(){
-digitalWrite(A_1,HIGH) ;
-digitalWrite(A_2,LOW) ;
-
-digitalWrite(A_1,HIGH) ;
-digitalWrite(A_2,LOW) ;
-}
-*/
